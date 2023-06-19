643b318913405dff8762d694ee6d8c3930e75e3b
Neeraj Kumar <nk7075034@gmail.com>
2023-06-19 16:41:57 +0530

    Improved the design of the futuristic button

--- Futuristic Button/Futuristic_Button_Effect.js
+++ Futuristic Button/Futuristic_Button_Effect.js
@@ -4,9 +4,6 @@ futuristic_buttons.forEach((button) => {
     button.addEventListener("mousemove", (event) => {
         const button_rectangle = button.getBoundingClientRect();
         const mouseX = event.clientX;
-        const mouseY = event.clientY;
-
-        const angle = Math.atan((button_rectangle.left - mouseX) / (button_rectangle.top - mouseY));
-        button.style.setProperty("--angle", angle * 360 + "deg");
+        button.style.setProperty("--angle", (mouseX - button_rectangle.left) + "deg");
     });
 });
